7e9e45ed566931fb852edf020947ff8ec81e9a15
Arielegend <autologisr@gmail.com>
2020-03-27 04:34:04 +0300

    added auth and api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Register from './components/Auth/Register';
+import React from 'react';
+
+
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
+
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+// import Container from '@material-ui/core/Container';
+
+// import  { withAuthenticator } from 'aws-amplify-react'
+
+// import aws_exports from './aws-exports'
+// import Amplify from 'aws-amplify'
+
+// import useStyles from './components/styles';
+// import Home from './components/Home';
+
+
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Link to="/">Home Page</Link> <Link to="/about">About Page</Link> <Link to="/register">Register </Link>
+      </div>
+      <h2>  welcome to AutoLog  </h2>
+      <Switch>
+        <Route exact path="/">
+          <h1>Home Page</h1>
+        </Route>
+        <Route exact path="/about">
+          <h1>About Page</h1>
+        </Route>
+        <Route exact path="/register">
+          <Register />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
-}
+};
 
+// export default withAuthenticator(App, true);
 export default App;
+
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+import Amplify, { Auth } from 'aws-amplify';
+import config from './config.json';
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+});
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
